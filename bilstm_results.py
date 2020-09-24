04dc853a647ea3dce763affbad62c57daa46a819
Kysha <33337655+llinzy@users.noreply.github.com>
2020-09-24 13:55:10 -0400

    Update bilstm_results.py

--- bilstm_results.py
+++ bilstm_results.py
@@ -4,7 +4,7 @@ import streamlit as st
 df_encoded_=pd.read_csv('df_words_matched.csv')
 output_df=pd.read_csv('output_df.csv')
 
-st.write('Prediction Results for Bidirectional Long-term Short-term Memory (BiLSTM))
+st.write('Prediction Results for Bidirectional Long-term Short-term Memory (BiLSTM)')
 
 st.subheader('Prediction Results Loss & Accuracy Report')
 st.write(output_df)
