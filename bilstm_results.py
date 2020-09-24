835a14d29e9f587fd7370ab105e093b104055c01
Kysha <33337655+llinzy@users.noreply.github.com>
2020-09-24 13:44:48 -0400

    Update bilstm_results.py

--- bilstm_results.py
+++ bilstm_results.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import streamlit as st
 
-df_encoded_=pd.read_csv('df_encoded_.csv')
+df_encoded_=pd.read_csv('df_words_matched.csv')
 output_df=pd.read_csv('output_df.csv')
 
-st.write('Predict Skill Category with Bidirectional Long-term Short-term Memory (biLSTM)')
+st.write('Prediction Results for Bidirectional Long-term Short-term Memory (BiLSTM))
 
-st.subheader('Prediction Result Loss & Accuracy Report')
+st.subheader('Prediction Results Loss & Accuracy Report')
 st.write(output_df)
 
-st.subheader('Results Data')
+st.subheader('Predicted Skill Category for each ID')
 df_cat_default_type=st.selectbox('Select ID', list(df_encoded_.id.unique()))
                           
 df_cat_df=df_encoded_[df_encoded_.id==df_cat_default_type]
 
-st.write(df_cat_df.iloc[:,:2])     
+st.write(df_cat_df.iloc[:,:2])
+         
+st.subheader('Words Matched to Predicted Skill Category')
 st.write(df_cat_df.iloc[:,3])
