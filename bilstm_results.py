44c9a618768fab4e6cd8401ff6fa1661273a836b
Kysha <33337655+llinzy@users.noreply.github.com>
2020-11-21 16:23:36 -0500

    Update bilstm_results.py

--- bilstm_results.py
+++ bilstm_results.py
@@ -21,4 +21,5 @@ st.subheader('Words Matched to Predicted Skill Category')
 words_matched=pd.concat([pd.Series(list(range(1,len(df_cat_df.iloc[:,4])+1)),name='index_'), 
            pd.Series(df_cat_df.iloc[:,4],name='word').explode()],
           axis=1)
-st.write(''.join(str(words_matched.word)).split(','))
+words_matched=words_matched.fillna('None')
+st.write(''.join(words_matched.word).split(','))
